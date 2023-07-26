f6bdb893f5e31f03c26a556fc8b9e1a988b466e8
Dev <Dev@DESKTOP-FE98N7K>
2023-07-26 12:18:19 +0530

    FIRST COMMIT

--- day3/src/org/tnsif/classandobject/LibraryExecutor.java
+++ day3/src/org/tnsif/classandobject/LibraryExecutor.java
@@ -2,6 +2,7 @@ package org.tnsif.classandobject;
 public class LibraryExecutor {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("unused")
 		Library l = new Library();
 		Library l1 = new Library(5,"Echiro oda ","one piece",(float) 15.3);
 		System.out.println(l1);
